e2d96c5ee554de8f747567955501d90efac5ef85
Ephraim Kunz <ephraimkunz@me.com>
2023-09-15 07:40:09 -0600

    Initial Commit

---
+++ StudentOutreach/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudentOutreach
+//
+//  Created by Ephraim Kunz on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudentOutreach/StudentOutreachApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentOutreachApp.swift
+//  StudentOutreach
+//
+//  Created by Ephraim Kunz on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StudentOutreachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
